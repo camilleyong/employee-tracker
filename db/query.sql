e7e3b050f84faa7e9cd61c7e429219b3bc41f928
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2022-10-03 13:35:34 -0700

    added arrays

--- db/query.sql
+++ db/query.sql
@@ -8,13 +8,13 @@ LEFT JOIN departments ON roles.department_id = departments.id;
 
 -- EMPLOYEES
 SELECT employees.id,
-       employee.first_name,
-       employee.last_name,
+       employees.first_name,
+       employees.last_name,
        roles.title AS title,
        departments.department_name AS department,
        CONCAT (manager.first_name, " ", manager.last_name) AS manager
 FROM employees 
 LEFT JOIN roles ON employees.role_id = roles.id
 LEFT JOIN departments ON roles.department_id = departments.id
-LEFT JOIN employees ON employees.manager_id = manager.id;
+LEFT JOIN employees manager ON manager.id = employees.manager_id;
 
--- server.js
+++ server.js
@@ -4,29 +4,20 @@ const start = require("./lib/employee.js");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const init = new Employee();
+// const init = new Employee();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: 'sprite95',
-      database: 'employee_db'
-    },
-    console.log(`Connected to the employee_db database.`)
-  );
 
-  app.use((req, res) => {
+app.use((req, res) => {
     res.status(404).end();
   });
   
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT} 🚀`);
     start();
   });
 
-
+// init.start();
   
